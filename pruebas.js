621a240bd62cc9352e472491ee462d5d8492df0d
Joberto Fanchini <jobertofanchini@gmail.com>
2023-05-10 10:09:20 -0300

    ...

--- pruebas.js
+++ pruebas.js
@@ -3,3 +3,10 @@ function multiplicar (num1, num2) {
 }
 
 console.log(multiplicar(2,3));
+
+
+function multiplicar_segunda (num1, num2) {
+    console.log(num1 * num2);     
+}
+
+multiplicar_segunda(2,3);
